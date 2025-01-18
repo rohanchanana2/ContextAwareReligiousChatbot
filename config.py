253ec78c6d382245f77d4410163be8592c624804
rohanchanana2 <rohanchanana2@gmail.com>
2025-01-18 16:55:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 import google.generativeai as genai
 
 # Add your API key in a '.env' file
@@ -6,6 +6,6 @@ import google.generativeai as genai
 # Configure API and model
 def configure_api_and_model(api_key):
     genai.configure(api_key=api_key)
-    instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
+    instructor_embeddings = HuggingFaceEmbeddings(model_name="hkunlp/instructor-large")
     model = genai.GenerativeModel("gemini-1.5-flash")
     return instructor_embeddings, model
